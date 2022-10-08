7e03d250d60c5124c2f0c5583c5a7637ded83f39
sarika <115362051+sarika-dataengineer@users.noreply.github.com>
2022-10-08 21:59:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,10 @@ import streamlit
 
 streamlit.title('My Moms New Healthy Diner')
 
-streamlit.header('Breakfast Favorites')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Klae,Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.text('Avacado Toast')
+🍞streamlit.header('Breakfast Favorites')
+🥪streamlit.text('Omega 3 & Blueberry Oatmeal')
+🧋streamlit.text('Klae,Spinach & Rocket Smoothie')
+🍳streamlit.text('Hard-Boiled Free-Range Egg')
+🥂streamlit.text('Avacado Toast')
+
+🍉🥝streamlit.header('Bulid Your Own Fruit Smoothie')🍋🥝🍍
