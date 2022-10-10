524ecc3ea1d0a1033c6a0eb4c05cbd3fb8043c52
sarika <115362051+sarika-dataengineer@users.noreply.github.com>
2022-10-10 21:06:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,6 @@ streamlit.text('🥂Avacado Toast')
 
 streamlit.header('🍉🥝Bulid Your Own Fruit Smoothie🍋🥝🍍')
 
-
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
